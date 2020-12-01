0108ec6cd64499cffdfa430b208d899684060b6d
Kelifarn <axac@list.ru>
2020-12-01 17:57:20 +0600

    beginning

---
+++ app/src/main/java/gms/appk/MainActivity.java
@@ -0,0 +1,13 @@
+package gms.appk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
