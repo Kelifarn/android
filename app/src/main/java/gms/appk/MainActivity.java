11731b69112584319a4f4c48bd225ee83ae0df5e
Kelifarn <axac@list.ru>
2020-12-22 20:40:39 +0600

    BeerExpert completed!

--- app/src/main/java/gms/appk/MainActivity.java
+++ app/src/main/java/gms/appk/MainActivity.java
@@ -2,12 +2,34 @@ package gms.appk;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    private BeerExpert opts = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Вызывается при щелчкке на кнопке
+    public void onClickFindBeer(View view) {
+        //Получить ссылку на TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Получить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Получить вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Получить рекомендации от класса BeerExpert
+        List<String> brandsList = opts.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Вывести сорта пива
+        brands.setText(brandsFormatted);
     }
 }
